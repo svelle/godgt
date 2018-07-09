ffe526721bd21c58d224f7405fb498afc8dc19ea
Sven Huester <sven.svelle@gmail.com>
2018-07-09 13:56:34 +0200

    loggin options, fixed counting

--- chess_log/chess_log.go
+++ chess_log/chess_log.go
@@ -13,18 +13,17 @@ import (
 
 var opts struct {
 	Pngs bool   `long:"pngs" description:"Write PNG images of board updates"`
+	Log bool	`long:"log" description:"Write Log output to console"`
 	Port string `short:"p" long:"port" description:"Serial port" default:"/dev/ttyUSB0" env:"DGT_PORT"`
-
 	Size int `short:"s" long:"size" description:"Image size" default:"128"`
-
 	Filename string `short:"f" long:"filename" description:"File prefix for png image files" default:"boardupdate"`
 }
 
 type boardStatus struct {
-	MoveCount	int	`json:"moveCount"`
+	MoveCount   int       `json:"moveCount"`
 	Timestamp   time.Time `json:"timestamp"`
-	BoardUpdate string `json:"boardUpdate"`
-	ImageFile   string `json:"imageFile"`
+	BoardUpdate string    `json:"boardUpdate"`
+	ImageFile   string    `json:"imageFile"`
 }
 
 func main() {
@@ -42,23 +41,12 @@ func main() {
 	dgtboard.WriteCommand(godgt.DGT_SEND_UPDATE_BRD)
 
 	go dgtboard.ReadLoop()
-
 	var messageCount int
 	var moveCount int
 
 	startTime := time.Now().Format("2006-01-02T15.04.05Z")
 	os.Mkdir(startTime, 0777)
 	logPath := "./" + startTime + "/log.json"
-
-	//fo, err := os.Create(logPath)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer func() {
-	//	if err := fo.Close(); err != nil {
-	//		panic(err)
-	//	}
-	//}()
 
 	var statusArray []boardStatus
 
@@ -68,15 +56,26 @@ func main() {
 			messageCount++
 			filename := fmt.Sprintf("%s/%s-%04d.png",
 				startTime, opts.Filename, moveCount)
-
-			statusArray = append(statusArray, writeLog(message, filename, &moveCount))
-			b, err := json.Marshal(statusArray)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				err := ioutil.WriteFile(logPath, b, 0666)
-				if err != nil{
+			lastTurn, err := writeLog(message, filename, &moveCount)
+			if err == 0 {
+				statusArray = append(statusArray, lastTurn)
+				sa, err := json.Marshal(statusArray)
+				if err != nil {
 					log.Fatal(err)
+				} else {
+					// print log to console if flag is set
+					if opts.Log {
+						lt, err := json.MarshalIndent(lastTurn, "", "\t")
+						if err != nil {
+							log.Fatal(err)
+						} else {
+							fmt.Println(string(lt))
+						}
+					}
+					err := ioutil.WriteFile(logPath, sa, 0666)
+					if err != nil {
+						log.Fatal(err)
+					}
 				}
 			}
 			if opts.Pngs && message.BoardUpdate != nil {
@@ -98,7 +97,7 @@ func main() {
 	}
 }
 
-func writeLog(m *godgt.Message, fileName string, mc *int) boardStatus{
+func writeLog(m *godgt.Message, fileName string, mc *int) (boardStatus, int) {
 	var currentStatus boardStatus
 	if m.BoardUpdate != nil {
 		*mc++
@@ -108,7 +107,7 @@ func writeLog(m *godgt.Message, fileName string, mc *int) boardStatus{
 			m.ToString(),
 			fileName,
 		}
-		return currentStatus
+		return currentStatus, 0
 	}
-	return currentStatus
+	return currentStatus, 1
 }
